957f623d573309c24af701b2004b7218c8654a1d
ScrolliStudio <info@scrolli.co>
2024-06-11 23:15:39 +0200

    deploy

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -2,22 +2,15 @@
 	<div
 		class="text-center md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full"
 	>
-		Get curated show <br class="hidden md:block" /> and movie recommendations
-		<br class="hidden md:block" /> with Open AI
+		Find your perfect movie  <br class="hidden md:block" /> <span style="color: #EF4444;">in seconds</span>, not minutes
+		<br class="hidden md:block" />
 	</div>
 	<div class="flex flex-wrap items-center mr-auto">
 		<button
 			on:click
-			class="w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6 mr-8"
+			class="w-full md:w-auto text-center bg-red-600 hover:bg-red-500 mb-4 flex-none rounded-full text-white font-medium py-3 px-6 mr-8"
 		>
 			Get recommendations
 		</button>
-		<a
-			href="https://www.youtube.com/watch?v=Z3LrfdctOpU"
-			target="_blank"
-			class="w-full md:w-auto text-center bg-white/20 hover:bg-white/50 mb-4 flex-none rounded-full text-white font-medium py-3 px-6"
-		>
-			Learn how it's built
-		</a>
 	</div>
 </div>
